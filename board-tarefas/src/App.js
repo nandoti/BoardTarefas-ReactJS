ac093553b121aa652d98f53546e0693abbf933f2
Fernando Tavares <nando.frontend@gmail.com>
2021-08-30 21:18:53 -0300

    :tada: Criado componentes

---
+++ board-tarefas/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./index.js";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Minhas Tarefas</h1>
+    </div>
+  );
+};
+
+export default App;
