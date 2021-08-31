86349ce2d201df5fb752fabaf05d3b695ceec70c
Fernando Tavares <nando.frontend@gmail.com>
2021-08-31 19:47:40 -0300

    :construction: Novas funções para os componentes.

--- board-tarefas/src/App.js
+++ board-tarefas/src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import Tasks from './components/Tasks';
+
 
 import "./index.js";
 import "./App.css";
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+    id: '1',
+    title: 'Estudar programação',
+    },
+    {
+      id: '2',
+      title: 'Ler Livros',
+      completed: true,
+    },
+    
+  ]);
+
   return (
-    <div className="container">
-      <h1>Minhas Tarefas</h1>
-    </div>
+    <>
+      <div className="container">
+        <Tasks tasks={tasks}/>
+     
+      </div>
+   
+    </>
   );
 };
 
